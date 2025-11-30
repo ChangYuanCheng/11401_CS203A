6c09972b3483df0d8ad903ecafb816197ecc1b83
1131540 <a0966930190@gmail.com>
2025-11-30 21:34:48 +0800

    add hash functions

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -15,12 +15,22 @@
 #include "hash_fn.hpp"
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+    // hash = (key * 2654435761) >> 16
+    // 這樣可以讓數值分佈較平均
+    unsigned int k = static_cast<unsigned int>(key);
+    unsigned int hash = k * 2654435761u;
     return key % m;  // basic division method
 }
 
 int myHashString(const std::string& str, int m) {
     unsigned long hash = 0;
-    // TODO: replace with your own design
+   {
+        // 正常版本 (DJB2 字串 hash)
+        // 從 5381 開始，對每個字元做 hash = hash * 33 + c
+        unsigned long h = 5381;
+        for (unsigned char c : str) 
+        h = h * 33 + c;
+    }
+        
     return static_cast<int>(hash % m);  // basic division method
 }
