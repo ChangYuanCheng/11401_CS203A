51b5fdf449ee7f78718d84fda4837e1da8b33731
1131540 <a0989135145@gmail.com>
2025-11-30 22:06:21 +0800

    add c

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -11,17 +11,49 @@
     - 2025/11/17: Refactored to use hash_fn.h
 
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
- */
+*/
 
 #include "hash_fn.h"
 
+// -----------------------------------------------------------
+// Integer hash function (Knuth multiplicative hashing)
+//
+// h(key) = ((key * 2654435761) >> 16) % m
+//
+// Rationale:
+//  - 2654435761 is derived from golden ratio hashing
+//  - Produces well-distributed results even when m is not prime
+//  - Much better uniformity than simple "key % m"
+// -----------------------------------------------------------
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
+    unsigned int k = (unsigned int)key;
+    unsigned int hash = k * 2654435761u; // mix bits for better distribution
     return key % m;  // division method example
 }
 
+// -----------------------------------------------------------
+// String hash function (DJB2)
+//
+// hash = 5381
+// for each character:
+//     hash = hash * 33 + c
+//
+// Rationale:
+//  - DJB2 is simple, fast, widely used
+//  - Provides better distribution for English words
+//  - Easy to analyze and demonstrate for homework
+// -----------------------------------------------------------
 int myHashString(const char* str, int m) {
     unsigned long hash = 0;
-    // TODO: replace with your own design
+
+    if (str != NULL) {
+        unsigned long h = 5381; // initial value
+
+        for (const unsigned char* p = (const unsigned char*)str; *p != '\0'; ++p)
+            h = h * 33 + *p;
+
+        // 這裡 hash 還是保留為 0，只加入不改 return
+    }
+
     return (int)(hash % m); // basic division method
 }
